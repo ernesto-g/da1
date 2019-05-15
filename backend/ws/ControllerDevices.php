8915181984f2f2cac6f5d6498143b9df4f7b3caf
Ernesto Gigliotti <ernestogigliotti@gmail.com>
2019-05-14 21:51:42 -0300

    post funcionando modificando la base de datos

--- backend/ws/ControllerDevices.php
+++ backend/ws/ControllerDevices.php
@@ -20,7 +20,19 @@ class ControllerDevices extends ControllerManager
 
 	public function post($f3)
 	{
-		$out = ["response"=>true,"idDevice"=>$_POST["id"],"state"=>$_POST["checked"]];
+		$stateValue = 0;
+		if($_POST["checked"]=="true")
+			$stateValue = 1;
+
+		$data = array(1=>$stateValue,2=>$_POST["id"]);
+		
+		$result = $this->db->exec("UPDATE Devices SET state=? WHERE id=?",$data);
+
+		if($result==1)
+			$out = ["response"=>true,"idDevice"=>$_POST["id"],"state"=>$_POST["checked"]];
+		else
+			$out = ["response"=>false,"msg"=>"Error modificando base de datos"];
+
 		echo(json_encode($out));
 	}
 
