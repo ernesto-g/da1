9d0f8caf84ab9539b4a2777225c94fdbdb969ab5
Ernesto Gigliotti <ernestogigliotti@gmail.com>
2019-05-18 15:33:08 -0300

    ejercicio 12 ok

--- pruebas/js/main.js
+++ pruebas/js/main.js
@@ -27,17 +27,125 @@ let r = m.miMetodo(2,"mensaje");
 console.log(r);
 */
 // error: interferes with parameter declaration
+/*
 class Animal {
-    move(dist = 0) {
+    move(dist: number = 0) {
         console.log(`Animal moved ${dist}.`);
     }
 }
+
 class Dog extends Animal {
     bark() {
         console.log('Woof! Woof!');
     }
 }
+
 let dog = new Dog();
 dog.bark();
 dog.move(10);
 dog.bark();
+*/
+/*
+let inc:Function = (x:number) => {return x+1;};
+let r:number = inc(6);
+console.log(r);
+*/
+/*
+let inc:Function = function(x:number){return x+1};
+let r:number = inc(6);
+console.log(r);
+*/
+/*
+class MyClass{
+    counter:number=0;
+    constructor(c:number){
+        this.counter = c;
+    }
+    incFactory():Function{
+        return function(){return this.counter+1;};
+    }
+    
+    incFactory():Function{
+        return () => {return this.counter+1;};
+    }
+}
+
+let m:MyClass = new MyClass(6);
+let f:Function = m.incFactory();
+
+console.log(f());
+*/
+/*
+//opcion 1
+let b:HTMLElement = document.getElementById("boton");
+b.addEventListener("click",()=>{alert("click!");});
+*/
+/*
+//opcion 2
+function configClick(id:string,callback:any):void {
+    let b:HTMLElement = document.getElementById(id);
+    b.addEventListener("click",()=>{callback();});
+}
+
+function evento():void{
+    alert("Evento!");
+}
+
+configClick("boton",evento);
+*/
+//opcion 3
+function configClick(id, callback) {
+    let b = document.getElementById(id);
+    b.addEventListener("click", () => { callback(); });
+}
+class MyClass {
+    constructor() {
+        this.msg = "click!";
+    }
+    evento() {
+        alert(this.msg);
+    }
+}
+let o = new MyClass();
+configClick("boton", o.evento);
+//configClick("boton",()=>(o.evento())); // solucion
+/*
+//opcion 4
+interface BtnListener{
+    handleClick():void;
+}
+
+function configClick(id:string,listener:BtnListener):void {
+    let b:HTMLElement = document.getElementById(id);
+    b.addEventListener("click",()=>{listener.handleClick();});
+}
+
+class MyClass implements BtnListener{
+    msg:string="click!";
+
+    handleClick():void{
+        alert(this.msg);
+    }
+}
+
+let o:MyClass = new MyClass();
+configClick("boton",o);
+*/
+/*
+// opcion 5
+function configClick(id:string,listener:EventListenerObject):void {
+    let b:HTMLElement = document.getElementById(id);
+    b.addEventListener("click",listener);
+}
+
+class MyClass implements EventListenerObject{
+    msg:string="Evento!";
+
+    handleEvent(evt:Event):void{
+        alert(this.msg);
+    }
+}
+
+let o:MyClass = new MyClass();
+configClick("boton",o);
+*/ 
--- pruebas/main.ts
+++ pruebas/main.ts
@@ -30,7 +30,7 @@ console.log(r);
 
 // error: interferes with parameter declaration
 
-
+/*
 class Animal {
     move(dist: number = 0) {
         console.log(`Animal moved ${dist}.`);
@@ -47,3 +47,119 @@ let dog = new Dog();
 dog.bark();
 dog.move(10);
 dog.bark();
+*/
+
+/*
+let inc:Function = (x:number) => {return x+1;};
+let r:number = inc(6);
+console.log(r);
+*/
+
+/*
+let inc:Function = function(x:number){return x+1};
+let r:number = inc(6);
+console.log(r);
+*/
+
+/*
+class MyClass{
+    counter:number=0;
+    constructor(c:number){
+        this.counter = c;
+    }
+    incFactory():Function{
+        return function(){return this.counter+1;};
+    }
+    
+    incFactory():Function{
+        return () => {return this.counter+1;};
+    }
+}
+
+let m:MyClass = new MyClass(6);
+let f:Function = m.incFactory();
+
+console.log(f());
+*/
+
+/*
+//opcion 1
+let b:HTMLElement = document.getElementById("boton");
+b.addEventListener("click",()=>{alert("click!");});
+*/
+
+/*
+//opcion 2
+function configClick(id:string,callback:any):void {
+    let b:HTMLElement = document.getElementById(id);
+    b.addEventListener("click",()=>{callback();});    
+}
+
+function evento():void{
+    alert("Evento!");
+}
+
+configClick("boton",evento);
+*/
+
+
+//opcion 3
+function configClick(id:string,callback:any):void {
+    let b:HTMLElement = document.getElementById(id);
+    b.addEventListener("click",()=>{callback();});    
+}
+
+class MyClass{
+    msg:string="click!";
+
+    evento():void{
+        alert(this.msg);
+    }    
+}
+
+let o:MyClass = new MyClass();
+configClick("boton",o.evento);
+//configClick("boton",()=>(o.evento())); // solucion
+
+
+/*
+//opcion 4
+interface BtnListener{
+    handleClick():void;
+}
+
+function configClick(id:string,listener:BtnListener):void {
+    let b:HTMLElement = document.getElementById(id);
+    b.addEventListener("click",()=>{listener.handleClick();});    
+}
+
+class MyClass implements BtnListener{
+    msg:string="click!";
+
+    handleClick():void{
+        alert(this.msg);
+    }    
+}
+
+let o:MyClass = new MyClass();
+configClick("boton",o);
+*/
+
+/*
+// opcion 5
+function configClick(id:string,listener:EventListenerObject):void {
+    let b:HTMLElement = document.getElementById(id);
+    b.addEventListener("click",listener);    
+}
+
+class MyClass implements EventListenerObject{
+    msg:string="Evento!";
+
+    handleEvent(evt:Event):void{
+        alert(this.msg);
+    }    
+}
+
+let o:MyClass = new MyClass();
+configClick("boton",o);
+*/
