e6232ebaf30e41dcb9da7385b25be80a56e5a6fb
Ernesto Gigliotti <ernestogigliotti@gmail.com>
2019-05-22 19:34:38 -0300

    se borraron archivos que no servian. se agrego readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Desarrollo de aplicaciones I. FIUBA
+
+Ejercicios y material para la materia Desarrollo de Aplicaciones I. FIUBA.
--- device.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-	$out = ["response"=>true,"idDevice"=>$_POST["id"],"state"=>$_POST["checked"]];
-	//echo("OK");
-	//print_r($_POST);
-	echo(json_encode($out));
-
-?>
