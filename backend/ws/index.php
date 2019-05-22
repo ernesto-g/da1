1f06f4648e378c59f94757f743cbec6a6480b128
Ernesto Gigliotti <ernestogigliotti@gmail.com>
2019-05-21 21:49:48 -0300

    agregue ejercicios de php

--- backend/ws/index.php
+++ backend/ws/index.php
@@ -1,5 +1,11 @@
 <?php
+
+
 $f3 = require('lib/base.php');
+
+$f3->set('AUTOLOAD','Controllers/');
+$f3->set('DEBUG', 3); // 0-3; 0=off, 3=way too much information
+
 $f3->route('GET /',
     function() {
         echo 'Hello, world!';
